47fbe9fc63f54b435b248e8faaa304de8a3a9e94
MAhmadHussain <mahamdhussain305@gmail.com>
2022-10-30 21:51:47 +0500

    update checkout form

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -15,23 +15,53 @@
         </div>
         @endif
 
-        <form action="{{ url('payment') }}" method="post" id="payment-form">
-            @csrf
-            <div class="form-row">
-                <p><input type="text" class="form-controll" name="amount" placeholder="Enter Amount" /></p>
-                <input id="card-holder-name" type="text">
-
-                <!-- Stripe Elements Placeholder -->
-                <div id="card-element"></div>
-
-                <button id="card-button">card-holder-name
-                    Process Payment
-                </button>
-
-                <!-- Used to display form errors. -->
-                <div id="card-errors" role="alert"></div>
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="donate-now__payment-info">
+                    <h3 class="donate-now__title">Payment info</h3>
+                    <form action="{{ url('payment') }}" method="post" id="payment-form">
+                        @csrf
+                        <div class="row">
+                            <div class="col-xl-6">
+                                <div class="donate-now__payment-info-input">
+                                    <input type="text" placeholder="Card number" name="number">
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="donate-now__payment-info-input">
+                                    <input type="text" placeholder="MM / YY" name="date" id="datepicker" class="hasDatepicker">
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="donate-now__payment-info-input">
+                                    <input type="text" placeholder="Card code ( CVC )" name="code">
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="donate-now__payment-info-input">
+                                    <input type="text" placeholder="Card Holder Name" name="cardholder-name">
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="donate-now__payment-info-input">
+                                    <input type="text" placeholder="Billing address" name="Billing">
+                                </div>
+                            </div>
+                            <div class="col-xl-6">
+                                <div class="donate-now__payment-info-input">
+                                    <input type="text" placeholder="City" name="City">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="donate-now__payment-info-btn-box">
+                            <button type="submit" class="thm-btn donate-now__payment-info-btn">Donate
+                                now</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
+            <div class="col-lg-5"></div>
+        </div>
     </div>
 </div>
 @endsection
